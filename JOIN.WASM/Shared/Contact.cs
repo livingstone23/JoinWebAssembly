71b1eda4f995ff10354ad2650bac234cae00c134
livingstone23 <livingstone23@gmail.com>
2021-09-22 09:55:15 +0200

    01- Inital load of component

---
+++ JOIN.WASM/Shared/Contact.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JOIN.WASM.Shared
+{
+    public class Contact
+    {
+        public int ContactId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Contact()
+        {
+
+        }
+
+        public Contact(int contactId, string firstName, string lastName)
+        {
+            this.ContactId = contactId;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+    }
+}
